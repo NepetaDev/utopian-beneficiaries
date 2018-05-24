e3dc71ed259af36dcf332f40703513fad2d6931b
Ominousness <ominousness@protonmail.com>
2018-05-24 17:51:17 +0200

    reduce indentation levels for a better readability

--- beneficiaries.js
+++ beneficiaries.js
@@ -24,47 +24,45 @@ function beneficiaries(input) {
   matches = reMessage.exec(input);
   
   // Check if the bot call is present and throw an error if it's not.
-  if (matches && matches.length == 2) {
-    // Check if a mention list is present.
-    if (matches[1]) {
-      var totalLength = 0; // Total length of the correct mention list, used to compare with the total length of the mention list to check for any unrelated text being present.
-      var str = matches[1]; // Mention list (including malformed mentions).
-
-      // Match all correct mentions until there are no more.
-      while (true) {
-        matches = reMentionList.exec(str);
-        if (!matches) break;
-        if (matches.length == 3) {
-          totalLength += matches[0].length; // Increase the total length of the correct mention list.
-          var username = matches[1];
-          if (Object.keys(users).includes(username)) throw 'One username can only appear once.';
-          if (Object.keys(users).length == 8) throw 'The maximum number of mentioned users is 8.';
-          
-          // Check if the mention we're parsing currently contains a weight.
-          if (matches[2]) {
-            var weight = matches[2];
-
-            // Check if the weight is a positive number.
-            if (isNaN(weight) || weight < 0) throw 'The text is not properly formatted.';
-            weight = parseFloat(weight); // Cast the String to float to avoid calculation errors.
-
-            usersWithWeightsArePresent = true; // Set the mode flag.
-            users[username] = weight;
-            sum += weight;
-          } else {
-            usersWithoutWeightsArePresent = true; // Set the mode flag.
-            users[username] = null;
-          }
-        } else {
-          throw 'The text is not properly formatted.';
-        }
-      }
-
-      // Compare the lengths. If they don't match then there are extra characters in the mention list string.
-      if (totalLength != str.length) throw 'The text is not properly formatted.';
-    }
-  } else {
+  if (!matches || matches.length != 2) {
     throw 'The text is not properly formatted.';
+  }
+
+  // Check if a mention list is present.
+  if (matches[1]) {
+    var totalLength = 0; // Total length of the correct mention list, used to compare with the total length of the mention list to check for any unrelated text being present.
+    var str = matches[1]; // Mention list (including malformed mentions).
+
+    // Match all correct mentions until there are no more.
+    while (true) {
+      matches = reMentionList.exec(str);
+      if (!matches) break;
+      if (matches.length != 3) throw 'The text is not properly formatted.';
+      
+      totalLength += matches[0].length; // Increase the total length of the correct mention list.
+      var username = matches[1];
+      if (Object.keys(users).includes(username)) throw 'One username can only appear once.';
+      if (Object.keys(users).length == 8) throw 'The maximum number of mentioned users is 8.';
+      
+      // Check if the mention we're parsing currently contains a weight.
+      if (matches[2]) {
+        var weight = matches[2];
+
+        // Check if the weight is a positive number.
+        if (isNaN(weight) || weight < 0) throw 'The text is not properly formatted.';
+        weight = parseFloat(weight); // Cast the String to float to avoid calculation errors.
+
+        usersWithWeightsArePresent = true; // Set the mode flag.
+        users[username] = weight;
+        sum += weight;
+      } else {
+        usersWithoutWeightsArePresent = true; // Set the mode flag.
+        users[username] = null;
+      }
+    }
+
+    // Compare the lengths. If they don't match then there are extra characters in the mention list string.
+    if (totalLength != str.length) throw 'The text is not properly formatted.';
   }
   
   // Check for mode-specific requirements.
